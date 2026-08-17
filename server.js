ef8df1b0f381b41b1c9b1e7fd78ac52c4cb4bac6
d4383300-cmd <d4383300@gmail.com>
2026-08-17 23:51:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,42 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const players = {};
+let players = {};
 
 io.on('connection', (socket) => {
-    players[socket.id] = {
-        id: socket.id,
-        x: 0, y: 1.7, z: 0,
-        rotationY: 0,
-        skinColor: '#' + Math.floor(Math.random()*16777215).toString(16),
-        shirtColor: '#' + Math.floor(Math.random()*16777215).toString(16)
-    };
+  players[socket.id] = { id: socket.id, x: 0, z: 0, yaw: 0, msg: '' };
+  
+  socket.emit('currentPlayers', players);
+  socket.broadcast.emit('newPlayer', players[socket.id]);
 
-    socket.emit('currentPlayers', { players, selfId: socket.id });
-    socket.broadcast.emit('newPlayer', players[socket.id]);
+  socket.on('playerMove', (data) => {
+    if (players[socket.id]) {
+      players[socket.id].x = data.x;
+      players[socket.id].z = data.z;
+      players[socket.id].yaw = data.yaw;
+      socket.broadcast.emit('playerMoved', players[socket.id]);
+    }
+  });
 
-    socket.on('playerMove', (data) => {
-        if (players[socket.id]) {
-            Object.assign(players[socket.id], data);
-            socket.broadcast.emit('playerMoved', players[socket.id]);
-        }
-    });
+  socket.on('chatMessage', (msg) => {
+    if (players[socket.id]) {
+      players[socket.id].msg = msg;
+      io.emit('chatMessage', { id: socket.id, msg: msg });
+    }
+  });
 
-    socket.on('syncTvVideo', (url) => {
-        io.emit('updateTvVideo', url);
-    });
-
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('playerDisconnected', socket.id);
-    });
+  socket.on('disconnect', () => {
+    delete players[socket.id];
+    io.emit('playerDisconnected', socket.id);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
